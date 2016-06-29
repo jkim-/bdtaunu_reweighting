de79272f5d02f731cbef0ce7cbc2166b0decf7a4
dchao <dchao@caltech.edu>
2016-06-29 11:43:58 -0700

    Moved test programs.

--- test/prepare_inputs.sql
+++
@@ -1,9 +0,0 @@
-CREATE MATERIALIZED VIEW ff_reweight_input_sigmc AS
-SELECT 
-  eid, 
-  mc_n_vertices, mc_n_edges, 
-  mc_from_vertices, mc_to_vertices, 
-  mc_lund_id, mcmass,
-  mcenergy, mcp3, mccosth, mcphi,
-  mcenergycm, mcp3cm, mccosthcm, mcphicm
-FROM framework_ntuples_sigmc INNER JOIN graph_sigmc USING (eid);
